a51de84080316709508d9d68c2b16e060050a214
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-02-10 02:04:24 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-// "use client";
 import PostForm from "../components/PostForm";
 import Tasks from "../components/Tasks";
-// import React, { useState, useEffect } from "react";
 
 async function getPosts() {
   const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
@@ -12,24 +10,6 @@ async function getPosts() {
 }
 
 export default async function Home() {
-  // const [post, setPost] = useState([]);
-
-  // useEffect(() => {
-  //   const getPosts = async () => {
-  //     try {
-  //       const res = await fetch("/api/getPosts");
-  //       if (res.ok) {
-  //         const jsonRes = await res.json();
-  //         console.log(jsonRes);
-  //         setPost(jsonRes);
-  //       }
-  //     } catch (err) {
-  //       console.log(err);
-  //     }
-  //   };
-  //   getPosts();
-  // }, []);
-
   const data = await getPosts();
 
   const jsonData = JSON.parse(data);
@@ -39,14 +19,23 @@ export default async function Home() {
     <div>
       <PostForm />
       <div className="sm:flex  sm:justify-center sm:items-baseline sm:flex-wrap max-w-7xl mx-auto mt-7">
-        {jsonData.reverse().map((post) => (
-          <Tasks
-            title={post.title}
-            content={post.content}
-            id={post.id}
-            timestamp={post.timestamp}
-          />
-        ))}{" "}
+        {jsonData
+          .reverse()
+          .map(
+            (post: {
+              title: string;
+              content: string;
+              id: number;
+              timestamp: string;
+            }) => (
+              <Tasks
+                title={post.title}
+                content={post.content}
+                id={post.id}
+                timestamp={post.timestamp}
+              />
+            )
+          )}{" "}
       </div>
     </div>
   );
