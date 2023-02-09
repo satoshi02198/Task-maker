c51d2a9e48c9043e1ca25dbce65e43ad6dacaf21
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-02-09 11:20:27 +0900

    satohsi

--- app/page.tsx
+++ app/page.tsx
@@ -1,46 +1,47 @@
-"use client";
-
+// "use client";
 import PostForm from "../components/PostForm";
 import Tasks from "../components/Tasks";
-import React, { useState, useEffect } from "react";
+// import React, { useState, useEffect } from "react";
 
-const Home = () => {
-  const [post, setPost] = useState([]);
+async function getPosts() {
+  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
+  if (!res.ok) {
+    console.log(res);
+  }
+  return res.json();
+}
 
-  useEffect(() => {
-    const getPosts = async () => {
-      try {
-        const res = await fetch("/api/getPosts");
-        if (res.ok) {
-          const jsonRes = await res.json();
-          console.log(jsonRes);
-          setPost(jsonRes);
-        }
-      } catch (err) {
-        console.log(err);
-      }
-    };
-    getPosts();
-  }, []);
+export default async function Home() {
+  // const [post, setPost] = useState([]);
 
-  // async function getPosts() {
-  //   const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
+  // useEffect(() => {
+  //   const getPosts = async () => {
+  //     try {
+  //       const res = await fetch("/api/getPosts");
+  //       if (res.ok) {
+  //         const jsonRes = await res.json();
+  //         console.log(jsonRes);
+  //         setPost(jsonRes);
+  //       }
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   getPosts();
+  // }, []);
 
-  //   return res.json();
-  // }
-
-  // const data: {
-  //   id: number;
-  //   title: string;
-  //   content: string;
-  //   timestamp: string;
-  // }[] = await getPosts();
+  const data: {
+    id: number;
+    title: string;
+    content: string;
+    timestamp: string;
+  }[] = await getPosts();
 
   return (
     <div>
       <PostForm />
       <div className="sm:flex  sm:justify-center sm:items-baseline sm:flex-wrap max-w-7xl mx-auto mt-7">
-        {post.reverse().map((post) => (
+        {data.reverse().map((post) => (
           <Tasks
             title={post.title}
             content={post.content}
@@ -51,6 +52,4 @@ const Home = () => {
       </div>
     </div>
   );
-};
-
-export default Home;
+}
