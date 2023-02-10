880d6088c408f2bd91d03e40e6d95179922cd692
satoshi02198 <awareness_yoga@yahoo.co.jp>
2023-02-10 12:06:11 +0900

    ha

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,11 @@
 import PostForm from "../components/PostForm";
-import Tasks from "../components/Tasks";
+import ShowTasks from "./ShowTasks";
 
-async function getPosts() {
-  const res = await fetch(`${process.env.BASE_URL}/api/getPosts`);
-  if (!res.ok) {
-    console.log(res.json());
-  }
-  return res.text();
-}
-
-export default async function Home() {
-  const data = await getPosts();
-
-  const jsonData = JSON.parse(data);
-  console.log(jsonData);
-
+export default function Home() {
   return (
-    <div>
+    <>
       <PostForm />
-      <div className="sm:flex  sm:justify-center sm:items-baseline sm:flex-wrap max-w-7xl mx-auto mt-7">
-        {jsonData
-          .reverse()
-          .map(
-            (post: {
-              title: string;
-              content: string;
-              id: number;
-              timestamp: string;
-            }) => (
-              <Tasks
-                title={post.title}
-                content={post.content}
-                id={post.id}
-                timestamp={post.timestamp}
-              />
-            )
-          )}{" "}
-      </div>
-    </div>
+      <ShowTasks />
+    </>
   );
 }
